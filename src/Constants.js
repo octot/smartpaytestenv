16b78cbebaeae16703d96e38a036fca6ff9c574a
Ajay Kumar <aajaykumarr32@gmail.com>
2025-12-29 20:57:25 +0530

    Change in backend url

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const URI ="https://smartnewpay.onrender.com/send-whatsapp"
+export const URI ="https://smartpaytestenv.onrender.com/send-whatsapp"
 // export const URI='http://localhost:4000/send-whatsapp'
--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,3 +1,10 @@
 // export const URI ="https://smartnewpay.onrender.com/send-whatsapp"
-export const URI='http://localhost:8080/send-whatsapp'
-export const baseURI='http://localhost:8080'
+// Local Env
+// export const URI='http://localhost:8080/send-whatsapp'
+// export const baseURI='http://localhost:8080'
+
+
+// https://smartpaytestenv.onrender.com/
+
+export const URI='https://smartpaytestenv.onrender.com/send-whatsapp'
+export const baseURI='https://smartpaytestenv.onrender.com'
